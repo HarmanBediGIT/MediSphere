46003284a494e35a05f54c2aed39de37ceb2f084
HarmanBediGIT <30410228@students.federation.edu.au>
2025-03-27 11:52:41 +0530

    Update text.php

--- medisphere/text.php
+++ medisphere/text.php
@@ -1,17 +1,1 @@
-            <!-- Har sukh dukh ch tu sth ae mere 
-            Pta v nii lggya kdo aage enne nede
 
-            Tere bina mnu lgge sb adhoora
-            Saddi yaarii da rishta ae sbto pyaara
-
-            Aakha kivn modya kra mere sohne sardaar da
-            Mnuu rkhya ae mehfoos apne puttn vaangra
-
-            Supne tere v honge sach, bas karla mehnt thodi hor
-            Ikk rabb ton te dujja apne ton, chaddi na bharose di dor 
-
-            Tere to hor kujj nii mngde sajjna
-            Bss hove tu kaamyaab, mere nl bhave mere to bina
-
-            Tadde nl ne sada m te mera pyr
-            Janamdin mubarak mere sohne sardaar -->
